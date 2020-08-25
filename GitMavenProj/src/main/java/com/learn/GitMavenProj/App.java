f71818cb4a2a1f86ceec4e8756c9d1adf3284dc2
narayanareddy451 <narayanareddy449@gmail.com>
2020-08-25 11:08:54 +0530

    Adding eclipse project to microservices

---
+++ GitMavenProj/src/main/java/com/learn/GitMavenProj/App.java
@@ -0,0 +1,13 @@
+package com.learn.GitMavenProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
