ac49e65547503ce588ea9448304ab618aadd1cbf
Narayanareddy451 <66298562+Narayanareddy451@users.noreply.github.com>
2020-08-25 11:16:45 +0530

    Update Test.java
    sysout changed

--- GitMavenProj/src/main/java/com/learn/GitMavenProj/Test.java
+++ GitMavenProj/src/main/java/com/learn/GitMavenProj/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testing is going on");
+		System.out.println("Testing is done woking fine =:)");
 	}
 
 }
